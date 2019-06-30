7e202e4cf3e25141411ab29f2d671cd280717806
Doug Niccum <dniccumdesign@gmail.com>
2019-06-30 09:08:48 -0500

    Adds logic to display the full state name if the abbreviations are being used.

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,9 +1,27 @@
 <template>
-    <panel-item :field="field" />
+    <panel-item :field="fieldData" />
 </template>
 
 <script>
 export default {
     props: ['resource', 'resourceName', 'resourceId', 'field'],
+    data() {
+        return {
+            fieldData: {}
+        }
+    },
+    mounted() {
+        this.fieldData = this.field;
+
+        if (this.field.abbreviations) {
+            let vm = this;
+
+            this.field.states.forEach(state => {
+                if (state.key === vm.field.value) {
+                    vm.fieldData.value = state.value;
+                }
+            })
+        }
+    }
 }
 </script>
--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,9 +1,27 @@
 <template>
-    <span>{{ field.value }}</span>
+    <span>{{ fieldData.value }}</span>
 </template>
 
 <script>
 export default {
     props: ['resourceName', 'field'],
+    data() {
+        return {
+            fieldData: {}
+        }
+    },
+    mounted() {
+        this.fieldData = this.field;
+
+        if (this.field.abbreviations) {
+            let vm = this;
+
+            this.field.states.forEach(state => {
+                if (state.key === vm.field.value) {
+                    vm.fieldData.value = state.value;
+                }
+            })
+        }
+    }
 }
 </script>
