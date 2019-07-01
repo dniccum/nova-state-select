bc198968402ada9e446c5fae0f07cd0fb0905d5b
Doug Niccum <dniccumdesign@gmail.com>
2019-06-30 22:00:14 -0500

    Replaces boilerplate with custom select code.

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,14 +1,13 @@
 <template>
     <default-field :field="field" :errors="errors">
         <template slot="field">
-            <input
-                :id="field.name"
-                type="text"
-                class="w-full form-control form-input form-input-bordered"
-                :class="errorClasses"
-                :placeholder="field.name"
-                v-model="value"
-            />
+            <select :id="field.name"
+                    class="w-full form-control form-input form-input-bordered"
+                    :class="errorClasses"
+                    v-model="value">
+                <option value="">-- {{ field.name }} --</option>
+                <option v-for="state of field.states" :value="state.key">{{ state.value }}</option>
+            </select>
         </template>
     </default-field>
 </template>
@@ -20,7 +19,6 @@ export default {
     mixins: [FormField, HandlesValidationErrors],
 
     props: ['resourceName', 'resourceId', 'field'],
-
     methods: {
         /*
          * Set the initial, internal value for the field.
