1cbb3faf59b42459a7f22934653e651c6a4d8f36
Doug Niccum <dniccumdesign@gmail.com>
2019-06-30 22:01:10 -0500

    Completes initial field code.

--- src/StateSelect.php
+++ src/StateSelect.php
@@ -13,6 +13,21 @@ class StateSelect extends Field
      */
     public $component = 'nova-state-select';
 
+    /**
+     * @var bool $useFullNames
+     */
+    private $useFullNames = false;
+
+    /**
+     * @var array|string[]
+     */
+    private $stateList = [];
+
+    /**
+     * @param $resource
+     * @param null $attribute
+     * @return mixed
+     */
     public function resolve($resource, $attribute = null)
     {
         $this->setAbbreviations();
@@ -25,16 +40,18 @@ class StateSelect extends Field
      *
      * @return void
      */
-    protected function setAbbreviations(): void
+    private function setAbbreviations(): void
     {
-        $abbreviations = collect($this->stateAbbreviations)->transform(function($state, $key) {
-            return new KeyValue($key, $state);
-        })->values()->toArray();
+        if (count($this->stateList) === 0) {
+            $this->stateList = collect($this->stateAbbreviations)->transform(function($state, $key) {
+                return new KeyValue($key, $state);
+            })->values()->toArray();
 
-        $this->withMeta([
-            'states' => $abbreviations,
-            'abbreviations' => true
-        ]);
+            $this->withMeta([
+                'states' => $this->stateList,
+                'abbreviations' => true
+            ]);
+        }
     }
 
     /**
@@ -44,13 +61,15 @@ class StateSelect extends Field
      */
     public function useFullNames(bool $useFullNames=true)
     {
+        $this->useFullNames = $useFullNames;
+
         if ($useFullNames) {
-            $fullStates = collect($this->fullStates)->transform(function($state) {
+            $this->stateList = collect($this->fullStates)->transform(function($state) {
                 return new KeyValue($state, $state);
             })->values()->toArray();
 
             return $this->withMeta([
-                'states' => $fullStates,
+                'states' => $this->stateList,
                 'abbreviations' => false
             ]);
         }
@@ -58,6 +77,92 @@ class StateSelect extends Field
         return $this;
     }
 
+    /**
+     * @return $this
+     */
+    public function includeTerritories()
+    {
+        if ($this->useFullNames) {
+            if (count($this->stateList) === 0) {
+                $this->useFullNames();
+            }
+
+            $territories = collect($this->fullTerritories)->transform(function($territory) {
+                return new KeyValue($territory, $territory);
+            })->values();
+        } else {
+            if (count($this->stateList) === 0) {
+                $this->setAbbreviations();
+            }
+
+            $territories = collect($this->territories)->transform(function($territory, $abbreviation) {
+                return new KeyValue($abbreviation, $territory);
+            })->values();
+        }
+
+        $this->stateList = collect($this->stateList)
+            ->merge($territories)
+            ->sortBy('value')
+            ->unique()
+            ->values()
+            ->toArray();
+
+        return $this->withMeta([
+            'states' => $this->stateList
+        ]);
+    }
+
+    public function customValues(array $customValues)
+    {
+        if ($this->useFullNames) {
+            if ($this->isAssoc($customValues)) {
+                return abort(500, 'The custom values provided cannot be an associative array.');
+            }
+            if (count($this->stateList) === 0) {
+                $this->useFullNames();
+            }
+
+            $customValueCollection = collect($customValues)->transform(function($value) {
+                return new KeyValue($value, $value);
+            })->values();
+        } else {
+            if (!$this->isAssoc($customValues)) {
+                return abort(500, 'The custom values must be an associative array.');
+            }
+            if (count($this->stateList) === 0) {
+                $this->setAbbreviations();
+            }
+
+            $customValueCollection = collect($customValues)->transform(function($value, $key) {
+                return new KeyValue($key, $value);
+            })->values();
+        }
+
+        $this->stateList = collect($this->stateList)
+            ->merge($customValueCollection)
+            ->sortBy('value')
+            ->unique()
+            ->values()
+            ->toArray();
+
+        return $this->withMeta([
+            'states' => $this->stateList
+        ]);
+    }
+
+    /**
+     * @param array $arr
+     * @return bool
+     */
+    private function isAssoc(array $arr)
+    {
+        if (array() === $arr) return false;
+        return array_keys($arr) !== range(0, count($arr) - 1);
+    }
+
+    /**
+     * @var array $stateAbbreviations
+     */
     private $stateAbbreviations = [
         'AL'=>'Alabama',
         'AK'=>'Alaska',
@@ -98,7 +203,6 @@ class StateSelect extends Field
         'OK'=>'Oklahoma',
         'OR'=>'Oregon',
         'PA'=>'Pennsylvania',
-//        'PR'=>'Puerto Rico',
         'RI'=>'Rhode Island',
         'SC'=>'South Carolina',
         'SD'=>'South Dakota',
@@ -113,6 +217,9 @@ class StateSelect extends Field
         'WY'=>'Wyoming'
     ];
 
+    /**
+     * @var array $fullStates
+     */
     private $fullStates = [
         'Alabama',
         'Alaska',
@@ -153,7 +260,6 @@ class StateSelect extends Field
         'Oklahoma',
         'Oregon',
         'Pennsylvania',
-//        'Puerto Rico',
         'Rhode Island',
         'South Carolina',
         'South Dakota',
@@ -166,5 +272,35 @@ class StateSelect extends Field
         'West Virginia',
         'Wisconsin',
         'Wyoming'
+    ];
+
+    private $territories = [
+        'AS' => 'American Samoa',
+        'DC' => 'District of Columbia',
+        'FM' => 'Federated States of Micronesia',
+        'GU' => 'Guam',
+        'MH' => 'Marshall Islands',
+        'MP' => 'Northern Mariana Islands',
+        'PW' => 'Palau',
+        'PR' => 'Puerto Rico',
+        'VI' => 'Virgin Islands',
+        'AE' => 'Armed Forces Africa',
+        'AA' => 'Armed Forces Americas',
+        'AP' => 'Armed Forces Pacific',
+    ];
+
+    private $fullTerritories = [
+        'American Samoa',
+        'District of Columbia',
+        'Federated States of Micronesia',
+        'Guam',
+        'Marshall Islands',
+        'Northern Mariana Islands',
+        'Palau',
+        'Puerto Rico',
+        'Virgin Islands',
+        'Armed Forces Africa',
+        'Armed Forces Americas',
+        'Armed Forces Pacific',
     ];
 }
